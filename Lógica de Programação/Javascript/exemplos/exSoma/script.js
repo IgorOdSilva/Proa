49ca30c840c20562f2168a0b95f96d1d110c828b
Igor Oliveira <127331783+IgorOdSilva@users.noreply.github.com>
2024-04-05 17:58:24 -0300

    calculo de soma js

---
+++ Lógica de Programação/Javascript/exemplos/exSoma/script.js
@@ -0,0 +1,7 @@
+// Exercicio Soma JavaScript
+
+var numero1 = parseInt(prompt("Digite o primeiro número: "));
+var numero2 = parseInt(prompt("Digite o segundo número: "));
+var soma = numero1 + numero2
+
+alert("O resultado é: " + soma)
