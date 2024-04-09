2cfd282b079092fb9e49ba8249e78e7ecdf6808c
Igor Oliveira <igornutrietec2020@gmail.com>
2024-04-09 17:35:17 -0300

    Exercicios de Lógica
    Exercicios de Lógica Adicionadas em JavaScript

---
+++ Lógica de Programação/Javascript/exemplos/exMes/script.js
@@ -0,0 +1,4 @@
+let dia = (prompt("Digite um número: "))
+let mensagem = (dia==1)?"Janeiro":(dia==2)?"Fevereiro":(dia==3)?"Março":(dia==4)?"Abril":(dia==5)?"Maio":(dia==6)?"Junho":(dia==7)?"Julho":(dia==8)?"Agosto":(dia==9)?"Setembro":(dia==10)?"Outubro":(dia==11)?"Novembro":(dia==12)?"Dezembro":"Número Inválido, DIGITE NOVAMENTE!"
+
+alert(mensagem)
---
+++ Lógica de Programação/Javascript/exemplos/exSemana/script.js
@@ -0,0 +1,4 @@
+let dia = (prompt("Digite um número: "))
+let mensagem = (dia==1)?"Domingo":(dia==2)?"Segunda-Feira":(dia==3)?"Terça-Feira":(dia==4)?"Quarta-Feira":(dia==5)?"Quinta=Feira":(dia==6)?"Sexta-Feira":(dia==7)?"Sábado":"Número inválido, TENTE NOVAMENTE!"
+
+alert(mensagem)
