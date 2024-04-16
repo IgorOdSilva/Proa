b6e61bd27ae3a436d2e85a2c015737ce289e0539
Igor Oliveira <igornutrietec2020@gmail.com>
2024-04-16 18:00:11 -0300

    arquivo react criado

---
+++ React/my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './homelander.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Reactnimismo
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React é demais!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
